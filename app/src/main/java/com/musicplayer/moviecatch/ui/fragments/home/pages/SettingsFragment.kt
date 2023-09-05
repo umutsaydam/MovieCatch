c38c293632842ef8704eb1e7e26bfd2b6f608cc5
Umut SAYDAM <umutsaydam24@gmail.com>
2023-09-05 10:02:04 +0300

    settings ui init

--- app/src/main/java/com/musicplayer/moviecatch/ui/fragments/home/pages/SettingsFragment.kt
+++ app/src/main/java/com/musicplayer/moviecatch/ui/fragments/home/pages/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.musicplayer.moviecatch.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,10 +17,27 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
+        initListeners()
+
         return binding.root
+    }
+
+    private fun initListeners() {
+        binding.themeLinear.setOnClickListener {
+            binding.switchTheme.toggle()
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.switchTheme.setOnCheckedChangeListener { compoundButton, b ->
+            Toast.makeText(context, "Changed", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.langLinear.setOnClickListener {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroy() {
